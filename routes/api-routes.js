d80cb3bdaa2b28bc40d38cd7c5d7f66024db6e39
Gerriss Gailey <gerrissgailey@gmail.com>
2021-04-03 23:17:28 -0600

    Adjusted a few things in api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,28 +2,30 @@
 const express = require("express")
 const notesData = require('../db/db');
 const fs = require("fs");
-
+const uuidv1 = require("uuid")
 
 console.log(notesData);
+
 module.exports = (app) => {
 
   app.get('/api/notes', (req, res) => {
     return res.json(JSON.parse(fs.readFileSync('./db/db.json')));
   });
-
+  
   app.post('/api/notes', (req, res) => {
     
     let addNote = req.body;
-
-    notesData.push(addNote);
-
-    addNote.id = notesData.indexOf(addNote);
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
-
-    res.json()
-  });
     
+    notesData.push(addNote);
+    
+    addNote.id = notesData.indexOf(addNote)+1;
+    
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
+    
+    return res.json(JSON.parse(fs.readFileSync('./db/db.json')));
+    // res.json()
+  });
+  
   app.delete('/api/notes/:id', (req, res) => {
     let id = parseInt(req.params.id);
     let deleteItem = notesData.filter(item => item.id != id);
@@ -32,6 +34,7 @@ module.exports = (app) => {
     
     fs.writeFileSync('./db/db.json', JSON.stringify(deleteItem));
     
-    res.json()
+    return res.json(JSON.parse(fs.readFileSync('./db/db.json')));
+    // res.json()
   });
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,4 +10,8 @@ module.exports = (app) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 };
